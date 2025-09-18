ba9118f10ff02105a3302ae4cb61a55ab2c7cd48
sakshivirhajobsearch <164556079+sakshivirhajobsearch@users.noreply.github.com>
2025-09-18 20:09:03 +0530

    Added

--- mysql/init_db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100)
-);
-
-INSERT INTO users (name, email) VALUES
-('Alice', 'alice@example.com'),
-('Bob', 'bob@example.com');
