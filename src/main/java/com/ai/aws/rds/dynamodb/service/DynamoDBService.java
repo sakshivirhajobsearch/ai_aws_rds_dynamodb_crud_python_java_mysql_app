737b298dafa8b7e7768824830aba7cfe4cbb305d
sakshivirhajobsearch <164556079+sakshivirhajobsearch@users.noreply.github.com>
2025-09-18 16:22:10 +0530

    Added

---
+++ src/main/java/com/ai/aws/rds/dynamodb/service/DynamoDBService.java
@@ -0,0 +1,33 @@
+package com.ai.aws.rds.dynamodb.service;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DynamoDBService {
+	
+	public static List<String> getDataFromDynamoDB() {
+		
+		return getData("http://localhost:5000/dynamodb");
+	}
+
+	private static List<String> getData(String urlStr) {
+		List<String> dataList = new ArrayList<>();
+		try {
+			URL url = new URL(urlStr);
+			System.out.println("Connecting to: " + urlStr);
+			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+			String inputLine;
+			while ((inputLine = in.readLine()) != null) {
+				dataList.add(inputLine);
+			}
+			in.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			dataList.add("Error: " + e.getMessage());
+		}
+		return dataList;
+	}
+}
